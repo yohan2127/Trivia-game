686d40636400915cc6e665da7333813ecbda6a25
yohan2127 <zedamek@yahoo.com>
2019-01-29 10:02:44 -0500

    fix broken image link

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -62,7 +62,7 @@ var triviaQuestions = [{
 	answer: 2
 }];
 
-var ImageArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10'];
+var ImageArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10', 'question11', 'question12', 'question13', 'question14', 'question15'];
 var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
 var messages = {
 	correct: "Yes, that's right!",
